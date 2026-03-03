5422a4d7b2fb8cc50528fe38a8f273e9aee8287b
regno01 <212147545+regno001@users.noreply.github.com>
2026-03-03 21:54:06 +0530

    Dictionary

---
+++ Dictionary/d1.py
@@ -0,0 +1,21 @@
+info = {
+    "key" : "value",
+    "name" : "Rahul",
+    "Learning" : "coding",
+     "age" : 35,
+    "is_Adult" : True,
+    "Marks" : 95.5,
+    "subject" : ["python", "c" , "java" ,],
+     "topics" :("dict","set")
+
+ }
+print(info["name"])
+print(info["subject"])
+print(info["topics"])
+#onordered , mutable
+print(type(info))
+
+null_dict={}
+
+null_dict["name"]="Ankita"
+print(null_dict)
---
+++ functions/fun1.py
@@ -0,0 +1,16 @@
+#function definition
+
+def sum(a,b): #parameters
+    sum = a+b
+    return print(sum)
+sum(1,2) #functionn call ; argumrnts
+
+
+#avg of three numbers
+def avg(a,b,c):
+
+
+    avg = (a+b+c)/3
+
+    return print(avg)
+avg(1,2,3)
